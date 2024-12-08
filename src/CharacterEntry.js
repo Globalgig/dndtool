f37a22ae60b036bda79cb35971d76328af24aa79
Alex Southard <00alexsouthard@gmail.com>
2024-12-08 13:22:42 -0700

    Initial configuration of app, adding characterentry/field

---
+++ src/CharacterEntry.js
@@ -0,0 +1,11 @@
+import './CharacterEntry.css';
+
+function CharacterEntry() {
+  return (
+    <div className="CharacterEntry">
+      <p>LOL YOU FOOL</p>
+    </div>
+  );
+}
+
+export default CharacterEntry;
---
+++ src/CharacterField.js
@@ -0,0 +1,16 @@
+import './CharacterField.css';
+import CharacterEntry from './CharacterEntry.js';
+
+
+function CharacterField() {
+  return (
+    <div className="CharacterField">
+      <CharacterEntry />
+      <CharacterEntry />
+      <CharacterEntry />
+      <CharacterEntry />  
+    </div>
+  );
+}
+
+export default CharacterField;
