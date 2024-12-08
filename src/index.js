b594de4a703128b05caaec56fda7b392d60430f2
Alex Southard <00alexsouthard@gmail.com>
2024-12-08 13:38:49 -0700

    Move character elements into subfolder, change dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CharacterField from './CharacterField'
+import CharacterField from './Character/CharacterField'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
