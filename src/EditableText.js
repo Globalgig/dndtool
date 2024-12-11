0f33a6a00b98261a1845c4accea4e35be970e03c
Globalgig <00alexsouthard@gmail.com>
2024-12-10 17:52:16 -0700

    Change lambdas, add todo

--- src/EditableText.js
+++ src/EditableText.js
@@ -5,14 +5,16 @@ function EditableText({initialText}) {
     const [isEditing, setIsEditing] = useState(false)
     const [text, setText] = useState(initialText)
   
-    const handleDoubleClick = e => {
+    // TODO: Change logic to auto-focus the input field on double click. e.target defaults to the <p> field currently
+    const handleDoubleClick = _ => {
+        // e.target.children[0].focus()
         setIsEditing(true);
     };
   
     return (
         <div className="Text" onDoubleClick={handleDoubleClick}>
           { isEditing ? (
-            <input type="text" value={text} onChange={e => setText(e.target.value)} onBlur={() => setIsEditing(false)} />
+            <input type="text" value={text} onChange={e => setText(e.target.value)} onBlur={_ => setIsEditing(false)} />
           ) : (
             <p>{text}</p>
           )}
